117df9a6eaa92eb2054955d5efdcfd4ce655ea86
Shadow32055 <shadow32055@gmail.com>
2024-01-16 13:18:47 -0500

    Fix localization

--- BetterExperience/Localizations/RefValues.cs
+++ BetterExperience/Localizations/RefValues.cs
@@ -1,6 +1,6 @@
 ﻿namespace BetterExperience.Localizations {
     public class RefValues {
-        public const string ExperienceText = "{BEE_OUkZom}Experience equation";
+        public const string ExperienceText = "{BEE_3KhOym}Experience values";
 
         public const string PowerText = "{=BEE_S7nfeK}Power";
         public const string PowerHint = "{=BEE_475wxq}Power value for experience equation (base*(level^power)";
